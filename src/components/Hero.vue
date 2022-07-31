3f4aa647d573ae8b36c9afb8a1a27ed217653c85
ekosutrisno1 <ekosutrisno801@gmail.com>
2022-07-31 17:36:17 +0700

    fix: issue margin and card height

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@
         Start Building Your <br> Wealth, Brick by <br class="lg:hidden"/> Brick
       </h1>
       <p class="font-semibold mt-5 lg:mt-6 text-briix-primary text-center lg:text-left text-base lg:text-2xl font-manrope leading-6 lg:leading-8">Briix keeps it simple, the way investing <br> should be.</p>
-      <p class="text-[10px] pl-[52px] lg:mx-0 lg:text-base mt-8">PT Briix Financial Technology is registered and supervised by</p>
+      <p class="text-[10px] pl-[52px] lg:pl-0 lg:mx-0 lg:text-base mt-8">PT Briix Financial Technology is registered and supervised by</p>
       <div class="flex mt-2 items-center justify-center lg:justify-start space-x-[12px]">
           <KominfoLogo />
           <img src="https://briix.com/images/products/fintech.png" height="32" width="58" alt="fintech-logo">
