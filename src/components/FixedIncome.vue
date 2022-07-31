faa8124846917391d739d882ff9cf284b7ae4c7e
ekosutrisno1 <ekosutrisno801@gmail.com>
2022-07-31 17:21:02 +0700

    update: finished all slicing design v1

---
+++ src/components/FixedIncome.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="lg:briix-container px-5 lg:h-[885px]">
+        <div class="grid lg:grid-cols-2">
+            <div class="relative">
+                <img class="absolute lg:left-24 lg:top-10 w-[176px] lg:w-auto top-[50px] left-20" src="https://briix.com/images/section-properties/Frame_1614.png" alt="property-img">
+                <img class="mt-[128px] lg:w-auto w-[260px] mx-auto" src="https://briix.com/images/section-properties/Group_427320939.svg" alt="property-decoration">
+            </div>
+            <div class="flex flex-col pb-[65px] lg:pb-0 lg:pt-32 items-center justify-center">
+                <div class="max-w-[604px]">
+                    <h1 class="text-2xl lg:text-[40px] font-manrope text-briix-black font-semibold leading-9 lg:leading-[60px]">Receive steady streams of <br> income with our fixed income <br> investments.</h1>
+                    <p class="mt-4 mb-6 text-briix-body font-inter font-normal leading-6 lg:leading-[30px]"> Our fixed income products provide users steady interest income throughout their tenure. All you have to do is just sit back, relax and watch your Briix wallet grow.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,19 @@
 <template>
-  <section id="hero-section" class="lg:h-[594px] briix-container bg-briix-blue-1 grid grid-cols-2">
+  <section id="hero-section" class="lg:h-[594px] briix-container bg-briix-blue-1 grid lg:grid-cols-2">
     <div>
-      <h1 class="font-semibold mt-[133px] text-5xl text-briix-blue-7 font-manrope leading-snug">
-        Start Building Your <br> Wealth, Brick by Brick
+      <h1 class="font-semibold text-center lg:text-left mt-10 lg:mt-[133px] text-4xl lg:text-5xl text-briix-blue-7 font-manrope leading-snug">
+        Start Building Your <br> Wealth, Brick by <br class="lg:hidden"/> Brick
       </h1>
-      <p class="font-semibold mt-6 text-briix-primary text-2xl font-manrope leading-8">Briix keeps it simple, the way investing <br> should be.</p>
-      <p class="text-base mt-8">PT Briix Financial Technology is registered and supervised by</p>
-      <div class="flex mt-2 items-center space-x-[12px]">
+      <p class="font-semibold mt-5 lg:mt-6 text-briix-primary text-center lg:text-left text-base lg:text-2xl font-manrope leading-6 lg:leading-8">Briix keeps it simple, the way investing <br> should be.</p>
+      <p class="text-[10px] pl-[52px] lg:mx-0 lg:text-base mt-8">PT Briix Financial Technology is registered and supervised by</p>
+      <div class="flex mt-2 items-center justify-center lg:justify-start space-x-[12px]">
           <KominfoLogo />
           <img src="https://briix.com/images/products/fintech.png" height="32" width="58" alt="fintech-logo">
           <OjkLogo />
       </div>
     </div>
     <div>
-      <Ilustration class="mt-10"/>
+      <Ilustration class="mx-auto -mt-10 lg:mt-10 w-[320px] lg:w-auto lg:max-w-[564px]"/>
     </div>
   </section>
 </template>
@@ -22,5 +22,4 @@
     import Ilustration from './svg/Ilustration.vue';
     import KominfoLogo from './svg/KominfoLogo.vue';
     import OjkLogo from './svg/OjkLogo.vue';
-
 </script>
